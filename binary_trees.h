6598920cf70d895f9b1039650ad3e184027251eb
Elzango <elzango.mzgmail.comgit config --global init.defaultBranch maingit config --global user.email elzango.mzgmail.com>
2023-05-31 12:14:28 +0100

    Add files to the project binary tree and updates the .h file

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,10 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 size_t binary_tree_height(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
+int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 #endif
